cdc1dda85ff1f000652f3946bf362af08d051c05
uboumedj <uboumedj@student.42.fr>
2023-06-30 02:19:30 +0200

    Transformation now works with directories too

--- Transformation.py
+++ Transformation.py
@@ -1,76 +1,119 @@
 import os, sys, filetype, random, fnmatch
 from plantcv import plantcv as pcv
 import numpy as np
+import click
 from datetime import datetime
 
 
-def usage():
-    print("usage:\n\tTransformation.py [path_to_img or path_to_directory]")
-
-
 def find(pattern, path):
+    """
+    Finds the file whose name matches the requested pattern
+    Arguments:
+        pattern (string): requested pattern
+        path (string): path where the file must be searched
+    Returns:
+        Complete path to the file, or None if file wasn't found
+    """
     for root, dirs, files in os.walk(path):
         for name in files:
             if fnmatch.fnmatch(name, pattern):
                 return os.path.join(root, name)
     return None
 
-def transform(img_path: str) -> None:
-    # img transformations with openCV
+def transform_image(img_path: str, dst: str) -> None:
+    """
+    Performs six image transformations on an image using PlantCV's functions
+    Arguments:
+        img_path (string): path to the image on which transformations are to be performed
+        dst (string): directory where resulting images will be stored
+    """
+    
+    # Initialisation
     img, path, filename = pcv.readimage(img_path)
-    pcv.params.debug_outdir = os.path.split(img_path)[0]
+    pcv.params.debug_outdir = dst
+    if not os.path.exists(dst):
+        os.makedirs(dst)
+    new_image_prefix = dst + '/' + (img_path[2:len(img_path) - 4] if img_path[0:2] == "./" else img_path[0:len(img_path) - 4])
+    new_image_directory = os.path.split(new_image_prefix)[0]
+    if not os.path.exists(new_image_directory):
+        os.makedirs(new_image_directory)
+
+    # Generating the black and white mask necessary for many transformations
     s = pcv.rgb2gray_hsv(rgb_img=img, channel='s')
     s_thresh = pcv.threshold.binary(gray_img=s, threshold=60, max_value=255, object_type='dark')
     mask = pcv.invert(s_thresh)
     mask = pcv.erode(gray_img=mask, ksize=3, i=1)
     
-    # gaussian blur
+    # Gaussian blur of mask
     gaussian_blur = pcv.gaussian_blur(mask, ksize=(3,3))
-    pcv.print_image(gaussian_blur, img_path[0:len(img_path) - 4]+"_PCVBLUR.JPG")
+    pcv.print_image(gaussian_blur, new_image_prefix + "_PCVBLUR.JPG")
     
-    # mask
+    # Masked image
     masked = pcv.apply_mask(img=img, mask=mask, mask_color='white')
-    pcv.print_image(masked, img_path[0:len(img_path) - 4]+"_MASKED.JPG")
+    pcv.print_image(masked, new_image_prefix + "_MASKED.JPG")
     
     # Objects and contours
     contour, hierarchy = pcv.roi.rectangle(img, 0, 0, img.shape[0], img.shape[1])
     objects, object_hierarchy = pcv.find_objects(img, mask)
     pcv.params.debug = 'print'
     pcv.roi_objects(img=img, roi_contour=contour, roi_hierarchy=hierarchy, object_contour=objects, obj_hierarchy=object_hierarchy, roi_type='partial')
-    generated_file = find("*_obj_on_img.png", pcv.params.debug_outdir)
-    os.rename(generated_file, img_path[0:len(img_path) - 4]+"_ROI_OBJECTS.JPG")
-    remove_trash_file = find("*_roi_mask.png", pcv.params.debug_outdir)
+    generated_file = find("*_obj_on_img.png", dst)
+    os.rename(generated_file, new_image_prefix + "_ROI_OBJECTS.JPG")
+    remove_trash_file = find("*_roi_mask.png", dst)
     os.remove(remove_trash_file)
     pcv.params.debug = 'None'
 
-    # Analyze object
+    # Analyse object
     obj, mask = pcv.object_composition(img=img, contours=objects, hierarchy=object_hierarchy)
     analyse_img = pcv.analyze_object(img, obj, mask)
-    pcv.print_image(analyse_img, img_path[0:len(img_path) - 4]+"_ANALYZED.JPG")
+    pcv.print_image(analyse_img, new_image_prefix + "_ANALYZED.JPG")
 
     # Pseudolandmarks
     pcv.params.debug = 'print'
     pcv.y_axis_pseudolandmarks(img=img, obj=obj, mask=mask, label="default")
-    generated_file = find("*_pseudolandmarks.png", pcv.params.debug_outdir)
-    os.rename(generated_file, img_path[0:len(img_path) - 4]+"_PSEUDOLANDMARKS.JPG")
+    generated_file = find("*_pseudolandmarks.png", dst)
+    os.rename(generated_file, new_image_prefix + "_PSEUDOLANDMARKS.JPG")
     pcv.params.debug = 'None'
 
     # Analyse colors
-    color_channels = pcv.analyze_color(rgb_img=img, mask=mask, hist_plot_type='all', label="default")
-    pcv.print_image(color_channels, img_path[0:len(img_path) - 4]+"_COLORS.JPG")
+    color_channels = pcv.analyze_color(rgb_img=img, mask=mask, colorspaces='all', label="default")
+    pcv.print_image(color_channels, new_image_prefix + "_COLORS.JPG")
 
 
-def main() -> None:
-    # set seed
+def transform_directory(src: str, dst: str) -> None:
+    for filename in os.listdir(src):
+        file_path = os.path.join(src, filename)
+        if os.path.isfile(file_path):
+            transform_image(img_path=file_path, dst=dst)
+        elif os.path.isdir(file_path):
+            transform_directory(src=file_path, dst=dst)
+
+
+@click.command()
+@click.option('--src', default=None, help='Directory on which to perform transformations')
+@click.option('--dst', default=None, help="Storage directory of the transformed data")
+@click.argument('file', required=False)
+def main(file, src, dst) -> None:
+
+    # Set random seed
     random.seed(datetime.now().timestamp())
-    # argument
-    if len(sys.argv) != 2:
-        return usage()
-    if os.path.isfile(sys.argv[1]) is False:
-        return print("Argument {} does not exist".format(sys.argv[1]))
-    if (filetype.guess(sys.argv[1]) == None or (filetype.guess(sys.argv[1])).extension != 'jpg'):
-        return print("Argument {} is not a jpeg img".format(sys.argv[1]))
-    transform(sys.argv[1])
+
+    if file is not None:
+        if os.path.isfile(file) is False:
+            return print(f"{file} does not exist or is not a file")
+        if (filetype.guess(file) == None or filetype.guess(file).extension != 'jpg'):
+            return print(f"{file} is not a jpeg image")
+        transform_image(file, dst='.')
+    elif (src is not None and dst is not None):
+        if os.path.isdir(src) is False:
+            return print(f"{src} does not exist or is not a directory")
+        transform_directory(src, dst)
+    else:
+        ctx = click.get_current_context()
+        click.echo(ctx.get_help())
+        print(f"You must either provide a [src] and [dst] directory, or a source file as arguments for the transformation")
+        ctx.exit()
+
 
 
 if __name__ == "__main__":
